1fbcda0043ee0840cf18196ccb26bbff0b5d55e2
tstepnik <tomasz.stepnik77@gmail.com>
2020-10-08 16:16:49 +0200

    first compit

---
+++ src/Employee.java
@@ -0,0 +1,26 @@
+public class Employee {
+
+    String name;
+    String lastName;
+
+    public Employee(String name, String lastName) {
+        this.name = name;
+        this.lastName = lastName;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
